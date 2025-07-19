c61ecb992fd716ca4427be54df946b77df8ad488
KANAN-Q <kananag-bp215@code.edu.az>
2025-07-19 13:49:47 +0400

    Created CategoriesController And GetAll Method

---
+++ SimpleAPI/Controllers/CategoriesController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using SimpleAPI.BL.Cache;
+using SimpleAPI.Core.Entities;
+using SimpleAPI.Core.Repository;
+
+namespace SimpleAPI.Controllers;
+
+[ApiController, Route("api/[controller]/[action]")]
+public class CategoriesController(IGenericRepository<Category> _repo, ICacheService _cache) : ControllerBase
+{
+    [HttpGet]
+    public async Task<IActionResult> GetAll()
+    {
+        string cacheKey = "Categories_GetAll";
+        var cacheData = await _cache.GetAsync<List<Category>>(cacheKey);
+        if (cacheData != null && cacheData.Any()) return Ok(cacheData);
+        var data = await _repo.GetAllAsync();
+        if (data is null || !data.Any()) return BadRequest();
+        await _cache.SetAsync(cacheKey,cacheData);
+        return Ok(data);
+    }
+}
